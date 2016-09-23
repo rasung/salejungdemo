c6e438cad13230fd6893d933d83d8d8084f2e582
tae seok <salejung2038@gmail.com>
2016-09-23 23:36:13 +0900

    t

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
   , function (err, result) {
       if (err) {
         // pass the error to the express error handler
-        console.log(1111111);
+        console.log(err);
       }
       console.log(result);
   });
